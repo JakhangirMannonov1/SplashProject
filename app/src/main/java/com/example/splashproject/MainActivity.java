b85740d6ad4e8597cb370a0711e7b9cba9ef7cfa
JAKHANGIR <joxamannonov@gmail.com>
2023-02-02 22:19:14 +0300

    commit

--- app/src/main/java/com/example/splashproject/MainActivity.java
+++ app/src/main/java/com/example/splashproject/MainActivity.java
@@ -1,16 +1,16 @@
 package com.example.splashproject;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initViews();
@@ -18,12 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
     void initViews(){
         Button button1 = findViewById(R.id.button1);
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openDetailActivity();
-            }
-        });
+        button1.setOnClickListener(view -> openDetailActivity());
     }
 
     void openDetailActivity(){
